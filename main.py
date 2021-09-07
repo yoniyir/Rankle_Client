bbbbfa737f882138caa45154d2be4007497c5ceb
yoniyir <48987715+yoniyir@users.noreply.github.com>
2021-09-07 23:01:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bson.json_util import dumps
 
 # db.<collection_name>.find_one() -> returns one object
 # db.<collection_name>.find() -> returns all object from collection ->
-# collection = db.collection_name.find() ---> dupms(list(collection)) --> JSON
+# collection = db.collection_name.find() ---> dumps(list(collection)) --> JSON
 
 # POST -> request.method == 'POST' --> request_dict = request.form.to_dict() --> request_dict['KEY'] = "Value"
 
@@ -20,12 +20,30 @@ app.secret_key = "testing"
 db = PyMongo(app).db
 
 
-@app.route("/", methods=["GET", "POST"])
-def welcome():
+
+@app.route("/add_group", methods=["POST"])
+def add_group():
+    if request.method == "POST":
+        group = request.form.to_dict()
+        groups = db.groups
+        if groups.find_one({"name":group["name"],"userId":group["userId"]}):
+            return f'The name {group["name"]} exists!'
+        else:
+            #to change to session
+            groups.insert(group)
+            return f'<h1>Sucssefully added {group["name"]}</h1>'
+    
+
+
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "POST":
         user = request.form.to_dict()
         users = db.users
-        if users.find_one({"username": user["username"], "password": user["password"]}):
+        if users.find_one({"username": user["username"]}):
             current_user = users.find_one({"username": user["username"], "password": user["password"]})
             session['id']=str(current_user['_id'])
             return f'<h1>Sucssefully logged in to {user["username"]}!</h1>'
@@ -37,26 +55,11 @@ def welcome():
         return "Send by post username and password"
 
 
-@app.route("/add_player", methods=["GET", "POST"])
-def add_player():
-    if request.method == "POST":
-        player = request.form
-        player = player.to_dict()
-        return f'Successfully added {player["name"]} {player["last"]}'
-    else:
-        return "Please send by POST"
-
 
 @app.route("/whoami")
 def whoami():
 
     return session
-
-
-@app.route("/all_players")
-def get_all_players():
-
-    return
 
 
 @app.route("/get_users")
@@ -67,13 +70,12 @@ def get_users():
     return users_json
 
 
-@app.route("/players", methods=["GET", "POST"])
-def players():
-    if request.method == "POST":
-        temp = request.form
-        str = ""
-        for key in temp:
-            str += f"The {key.capitalize()} is {temp[key].capitalize()} \n"
-        return str
-    else:
-        return "Get"
+@app.route("/get_groups")
+def get_user_groups():
+    userGroups = db.groups.find({"userId":session['id']})
+    return dumps(list(userGroups))
+
+
+@app.route("/")
+def hello_world():
+    return redirect('/login')
